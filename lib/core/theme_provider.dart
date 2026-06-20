a005da3393a1ade02b97ccbf803f1fb015c2377e
m3hm3tcan <m3hm3tcan@gmail.com>
2026-06-20 15:30:48 +0200

    fix for dark mode init

--- lib/core/theme_provider.dart
+++ lib/core/theme_provider.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeProvider extends ChangeNotifier {
+  ThemeProvider() {
+    _loadTheme();
+  }
+
   ThemeMode themeMode = ThemeMode.system;
 
   bool get isDark => themeMode == ThemeMode.dark;
 
-  void toggleTheme() async {
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedDarkMode = prefs.getBool("darkMode");
+
+    if (savedDarkMode == null) return;
+
+    themeMode = savedDarkMode ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+
+  Future<void> toggleTheme() async {
     final prefs = await SharedPreferences.getInstance();
 
     themeMode =
